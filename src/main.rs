6a8e0d1f9e5193674f42be6c71a08b5e63a00324
Anna Scholtz <anna@scholtzan.net>
2019-04-04 20:20:07 -0700

    Transform diffs to DataSpans

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 //! A plugin to show git diff in xi-editor.
+extern crate git2;
 extern crate xi_core_lib as xi_core;
 extern crate xi_plugin_lib;
 extern crate xi_rope;
-extern crate git2;
 
+use git2::{Commit, DiffOptions, ObjectType, Repository, Signature, Time};
+use git2::{DiffFormat, Error, Pathspec};
+use std::cmp::max;
 use std::path::Path;
 use std::str;
-use git2::{Repository, Signature, Commit, ObjectType, Time, DiffOptions};
-use git2::{Pathspec, Error, DiffFormat};
 
+use crate::xi_core::annotations::AnnotationType;
+use crate::xi_core::plugin_rpc::DataSpan;
+use crate::xi_core::rpc::CoreNotification;
 use crate::xi_core::ConfigTable;
+use git2::{DiffHunk, DiffLine};
+use serde_json::json;
 use xi_plugin_lib::{mainloop, ChunkCache, Plugin, View};
 use xi_rope::interval::Interval;
 use xi_rope::rope::RopeDelta;
-use crate::xi_core::rpc::CoreNotification;
 
-struct XiffPlugin {
-}
+struct XiffPlugin {}
 
 impl Plugin for XiffPlugin {
     type Cache = ChunkCache;
@@ -44,9 +48,30 @@ impl Plugin for XiffPlugin {
 
 impl XiffPlugin {
     fn new() -> XiffPlugin {
-        XiffPlugin {
+        XiffPlugin {}
+    }
 
+    fn lines_to_data_span(&self, lines: Vec<usize>, view: &mut View<ChunkCache>) -> Vec<DataSpan> {
+        let mut i = 0;
+        let mut spans: Vec<DataSpan> = Vec::new();
+
+        while i < lines.len() {
+            let start = lines.get(i).unwrap();
+
+            while i < lines.len() - 1 && lines.get(i + 1).unwrap() - 1 == *lines.get(i).unwrap() {
+                i = i + 1;
+            }
+
+            spans.push(DataSpan {
+                start: view.offset_of_line(*start).unwrap(),
+                end: view.offset_of_line(*lines.get(i).unwrap() + 1).unwrap() - 1,
+                data: json!(null),
+            });
+
+            i = i + 1;
         }
+
+        spans
     }
 
     fn update_diff(&mut self, view: &mut View<ChunkCache>, interval: Interval) {
@@ -59,14 +84,60 @@ impl XiffPlugin {
 
                 let diff = repo.diff_index_to_workdir(None, Some(&mut opts)).unwrap();
 
-                // todo: translate into annotations
-                diff.print(DiffFormat::Raw, |delta, _hunk, line| {
-                    eprintln!("- {:?}", str::from_utf8(line.content()).unwrap());
-                    true
-                });
+                let mut inserted_lines: Vec<usize> = Vec::new();
+                let mut deleted_lines: Vec<usize> = Vec::new();
+
+                diff.foreach(
+                    &mut |_, _| true,
+                    None,
+                    None,
+                    Some(&mut |_delta, _hunk, line: DiffLine| {
+                        match line.origin() {
+                            '+' => {
+                                inserted_lines.push(line.new_lineno().unwrap() as usize);
+                            }
+                            '-' => {
+                                deleted_lines.push(line.old_lineno().unwrap() as usize);
+                            }
+                            _ => {}
+                        };
+
+                        true
+                    }),
+                );
+
+                let (modified_lines, inserted_lines): (Vec<usize>, Vec<usize>) = inserted_lines
+                    .into_iter()
+                    .partition(|l| deleted_lines.contains(l));
+
+                let (_, deleted_lines) = deleted_lines
+                    .into_iter()
+                    .partition(|l| modified_lines.contains(l));
+
+                let inserted_spans = self.lines_to_data_span(inserted_lines, view);
+                let deleted_spans = self.lines_to_data_span(deleted_lines, view);
+                let modified_spans = self.lines_to_data_span(modified_lines, view);
+
+                view.update_annotations(
+                    interval.start(),
+                    interval.end(),
+                    &inserted_spans,
+                    &AnnotationType::Other("added".to_string()),
+                );
+                view.update_annotations(
+                    interval.start(),
+                    interval.end(),
+                    &deleted_spans,
+                    &AnnotationType::Other("deleted".to_string()),
+                );
+                view.update_annotations(
+                    interval.start(),
+                    interval.end(),
+                    &modified_spans,
+                    &AnnotationType::Other("modified".to_string()),
+                );
             }
         }
-//        view.update_annotations(interval.start(), interval.end(),spans, annotation_type);
     }
 }
 
